abfaacfacd6a5046d03aa198b2e8e14cbb79eebb
RCIV <rayscook@gmail.com>
2023-03-02 20:50:37 -0500

    Added footer, changed styling on contact form

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import style from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <p>Copyright 2023 Ray Cook</p>
-    );
+            <div className={style['footer-cont']}>
+                <a href="https://www.twitter.com/rcivnft" target="_blank" rel="noreferer" className={style['footer-icon']}><i className="fa-brands fa-twitter fa-2xl"></i></a>
+                <a href="https://www.linkedin.com/in/raymond-cook-iv-4ba20138/" target="_blank" rel="noreferer" className={style['footer-icon']}><i className="fa-brands fa-linkedin fa-2xl"></i></a>
+                <a href="https://github.com/rc-iv" target="_blank" rel="noreferer" className={style['footer-icon']}><i className="fa-brands fa-github fa-2xl"></i></a>
+                <p>Copyright 2023 Ray Cook</p>
+            </div>
+    )
+        ;
 }
 
 export default Footer;
